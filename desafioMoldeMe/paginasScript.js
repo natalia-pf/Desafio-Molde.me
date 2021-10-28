288cbadf9ac9cb518f94201326f458e96dc78970
Natália Pisetta Fiorentin <82157666+natalia-pf@users.noreply.github.com>
2021-10-28 17:26:39 -0300

    Update paginasScript.js

--- desafioMoldeMe/paginasScript.js
+++ desafioMoldeMe/paginasScript.js
@@ -1,15 +1,9 @@
 const myForm = document.getElementById("myForm")
-var nome = document.getElementById("name")
-var email = document.getElementById("email")
-var aniversario = document.getElementById("birthday")
-var telefone = document.getElementById("number")
-var mensagem = document.getElementById("message")
-var arquivo = document.getElementById("labelFile")
+const modalContainer = document.getElementById("modalContainer")
 
 function openModal(){
     modalContainer.style.opacity = "1";
     modalContainer.style.pointerEvents = "auto";
-
 }
 
 function closeModal(){
